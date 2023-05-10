981dd2f3811b98a28865bd5ba92712693eb6fcc0
aleizn <119465229+aleizn@users.noreply.github.com>
2023-05-10 14:10:12 -0500

    Aleizn

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('Mystic - Bot\nWhatsApp Bot MD', {
+say('Aleizn-MD', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Bot creado por Bruno Sobrino`, {
+say(`Bot creado por Aleizn-MD 51992621601`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
